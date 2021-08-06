2122dd53a21334ed9c468ff8a866c97f0048733b
Maciek Z <zawi1997@gmail.com>
2021-08-06 07:44:36 +0200

    uio

--- main.ino
+++ main.ino
@@ -5,11 +5,11 @@
 #include <ModbusIP_ESP8266.h>
 #include <FastLED.h>
 
-#include <WiFi_login.h>
+//#include <WiFi_login.h>
 
-#define LED_PIN  3
+#define LED_PIN  5
 #define COLOR_ORDER GRB
-#define CHIPSET     WS2811
+#define CHIPSET     WS2812B
 #define BRIGHTNESS 32
 
 // Modbus Registers Offsets
@@ -17,16 +17,20 @@ const int MODE_HREG = 1;
 const int LENGTH_HREG = 2;
 const int PIXEL_HREG = 10;
 // Params for width and height
-const uint8_t kMatrixWidth = 1;
+const uint8_t kMatrixWidth = 8;
 const uint8_t kMatrixHeight = 1;
 
 #define NUM_LEDS (kMatrixWidth * kMatrixHeight)
-CRGB leds_plus_safety_pixel[ NUM_LEDS + 1];
-CRGB* const leds( leds_plus_safety_pixel + 1);
+struct CRGB leds[NUM_LEDS];
+//CRGB* const leds( leds_plus_safety_pixel + 1);
 
 //ModbusIP object
 ModbusIP mb;
-  
+
+uint32_t test1[8] = {0xffffff, 0xfffdfd, 0xffa3a3, 
+0xff2929, 0xff2929, 0xff2929, 0xff6a6a, 0xfff7f7};
+
+ 
 void setup() {
   Serial.begin(115200);
   WiFi.begin("Magnolia-WiFi", "14MaZa14!");
@@ -48,7 +52,7 @@ void setup() {
 
   mb.addHreg(MODE_HREG, 0x0001);
   mb.addHreg(LENGTH_HREG, 0x0000);
-  mb.addHreg(PIXEL_HREG, 0xABCD, 2);
+  mb.addHreg(PIXEL_HREG, 0x0000, 16);
   mb.addCoil(100, true); //test coil
   pinMode(LED_BUILTIN, OUTPUT); //test led
 }
@@ -60,9 +64,10 @@ bool status = mb.Coil(100);
 bool status1 = true;
     digitalWrite(LED_BUILTIN, status);
 
-    for (int i = 0; i < NUM_LEDS*2; i=i+2)
+    for (int i = 0; i < NUM_LEDS*2; i++)
     {
-        leds[i/2] = ((mb.Hreg(PIXEL_HREG + i)) << 7 | (mb.Hreg(PIXEL_HREG + i + 1)))
+        //leds[i] = ((mb.Hreg(PIXEL_HREG + i*2)) << 7 | (mb.Hreg(PIXEL_HREG + i*2 + 1)));
+        leds[i] = test1[i];
     }
     FastLED.show();
 
