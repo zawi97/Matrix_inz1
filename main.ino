3e31e016c94d14222308ee110017b87d57fbe2d9
Maciek Z <zawi1997@gmail.com>
2021-08-08 22:34:53 +0200

    display working

--- main.ino
+++ main.ino
@@ -5,11 +5,12 @@
 #include <ModbusIP_ESP8266.h>
 #include <FastLED.h>
 
+#include <Ticker.h>
 //#include <WiFi_login.h>
 
-#define LED_PIN  5
-#define COLOR_ORDER GRB
-#define CHIPSET     WS2812B
+#define LED_PIN  15
+#define COLOR_ORDER RGB
+#define CHIPSET     WS2812
 #define BRIGHTNESS 32
 
 // Modbus Registers Offsets
@@ -21,16 +22,34 @@ const uint8_t kMatrixWidth = 8;
 const uint8_t kMatrixHeight = 1;
 
 #define NUM_LEDS (kMatrixWidth * kMatrixHeight)
-struct CRGB leds[NUM_LEDS];
+CRGB leds[NUM_LEDS];
 //CRGB* const leds( leds_plus_safety_pixel + 1);
 
 //ModbusIP object
 ModbusIP mb;
 
-uint32_t test1[8] = {0xffffff, 0xfffdfd, 0xffa3a3, 
-0xff2929, 0xff2929, 0xff2929, 0xff6a6a, 0xfff7f7};
+Ticker LEDdisp;
 
- 
+uint32_t test1[8] = {0xfffff0, 0xfffdfd, 0xffa3a3, 
+0xff2929, 0xff2929, 0xff2929, 0x006a00, 0x0000fa};
+uint32_t tempLED[NUM_LEDS];
+bool dispRdy = false;
+
+void dispLED (){
+  for (int i = 0; i < NUM_LEDS; i++)
+    {
+      tempLED[i] = ((mb.Hreg(PIXEL_HREG + i*2 )) << 16 | (mb.Hreg(PIXEL_HREG + i*2+1)));
+        Serial.println(tempLED[i], HEX);
+        leds[i] = tempLED[i];
+        //leds[i] = test1[i];
+    }
+FastLED.show();
+}
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////
 void setup() {
   Serial.begin(115200);
   WiFi.begin("Magnolia-WiFi", "14MaZa14!");
@@ -48,30 +67,30 @@ void setup() {
   mb.server();
 
   FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalSMD5050);
+  //FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
   FastLED.setBrightness( BRIGHTNESS ); 
 
   mb.addHreg(MODE_HREG, 0x0001);
   mb.addHreg(LENGTH_HREG, 0x0000);
-  mb.addHreg(PIXEL_HREG, 0x0000, 16);
+  mb.addHreg(PIXEL_HREG, 0x00ff, 16);
   mb.addCoil(100, true); //test coil
   pinMode(LED_BUILTIN, OUTPUT); //test led
+  LEDdisp.attach(1, dispLED);
 }
  
 void loop() {
    mb.task();
    delay(10);
 bool status = mb.Coil(100);
-bool status1 = true;
+
     digitalWrite(LED_BUILTIN, status);
 
-    for (int i = 0; i < NUM_LEDS*2; i++)
-    {
-        //leds[i] = ((mb.Hreg(PIXEL_HREG + i*2)) << 7 | (mb.Hreg(PIXEL_HREG + i*2 + 1)));
-        leds[i] = test1[i];
-    }
-    FastLED.show();
+    
+    
 
 }
+
+
 
 /*uint16_t XY( uint8_t x, uint8_t y)
 {
